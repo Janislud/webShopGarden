73c066517c002eddbc6df921039f3d6deeae2ae4
Janislud <janisludborzs2@gmail.com>
2023-12-28 13:00:34 +0200

    "adaptive category"

--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -29,7 +29,9 @@ const Categories = () => {
 
             <div className={classes.CategoryTextWrapper}>
                 <h2 className={classes.categoryDescription}>Categories</h2>
+       
                 <div className={classes.line}></div>
+        
                 <button className={classes.categoryBtn} onClick={toggleCategories}>
                     <a className={classes.categoryBtnDescription} href="#">{isExpanded ? 'All Categoris' : 'All Categoris'}</a>
                 </button>
@@ -41,11 +43,15 @@ const Categories = () => {
                         <div key={category.id} className={classes.categoryCard}>
                             <img className={classes.categoryImg} src={`http://localhost:3333${category.image}`} alt={category.title} />
                             <h2 className={classes.categoryCardText}>{category.title}</h2>
+                            
                         </div>
                     ))}
+                    
                 </div>
             )}
-
+                <button className={classes.categoryBtnAdaptive} onClick={toggleCategories}>
+                    <a className={classes.categoryBtnDescription} href="#">{isExpanded ? 'All Categoris' : 'All Categoris'}</a>
+                </button>
         </div>
     );
 };
