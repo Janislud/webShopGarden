fe0355c7238a44577d26376118e6b93332cc03e8
Janislud <janisludborzs2@gmail.com>
2024-01-12 17:52:28 +0200

    "route fix"

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import Footer from "./Layout/Footer";
 import Header from "./Layout/Header";
 import Main from "./Pages/MainPage";
 import Categorys from "./Pages/CategoryPage";
 import { ApiProvider } from "./contexts";
 
-
 function App() {
   return (
     <Router>
       <ApiProvider>
-      <div className="App">
-        <Header />
-        <Switch>
-          <Route path="/main">
-            <Main />
-          </Route>
-          <Route path="/category">
-            <Categorys />
-          </Route>
-        </Switch>
-        <Footer />
-      </div>
+        <div className="App">
+          <Header />
+          <Switch>
+            <Route path="/main" component={Main} />
+            <Route path="/category" component={Categorys} />
+            <Redirect from="/" to="/main" />
+          </Switch>
+          <Footer />
+        </div>
       </ApiProvider>
     </Router>
   );
